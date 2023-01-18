7276a5e1dd1e9442b0f6be0d9493e2a5c0a23f69
Liam McHara <guillemlr@pm.me>
2023-01-18 17:18:22 +0100

    FIX dir_name variable & cleaner code

---
+++ h_crafter.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+name="lib_name"
+src_dir="./src"
+name_h=$(echo $name |  tr '[:lower:]' '[:upper:]' | sed 's/$/_H/g')
+h_file="$name.h"
+ft_names=$(find $src_dir -type f -name "*.c" -exec basename {} .c \;)
+
+echo "Getting function names..."
+rm -f $h_file
+vim -c ":Stdheader" -c ":wq" $h_file 
+
+echo "#ifndef $name_h" >> $h_file
+echo "# define $name_h" >> $h_file
+echo "// Insert includes here\n" >> $h_file
+
+for ft_name in $ft_names
+do
+	echo "$ft_name found!"
+	cat < $dir_name/$ft_name.c | grep "$ft_name(" | sed 's/$/;/g'>> $h_file
+done
+echo "\n#endif" >> $h_file
